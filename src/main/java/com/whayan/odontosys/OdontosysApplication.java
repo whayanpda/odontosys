f119b978266a20d8bdebbb92122a9d2fc806e019
Whayan <whayan20@gmail.com>
2026-05-22 15:03:03 -0300

    init: estrutura inicial do projeto com spring boot

---
+++ src/main/java/com/whayan/odontosys/OdontosysApplication.java
@@ -0,0 +1,13 @@
+package com.whayan.odontosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontosysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdontosysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whayan/odontosys/OdontosysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whayan.odontosys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontosysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
